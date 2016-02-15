9fc7682cd82190b20e608198563c61ff3489544e
Alejandro Ruiz Varela <elgoberlivel@gmail.com>
2016-02-15 12:38:35 -0600

    Trash Files Removed

--- Arduino/OTGLibrary/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,2 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.hoho.android.usbserial.driver", Managed="Com.Hoho.Android.Usbserial.Driver")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.hoho.android.usbserial.util", Managed="Com.Hoho.Android.Usbserial.Util")]
--- MJPGApp/BindingClass/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,1 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "com.camera.simplemjpeg", Managed="Com.Camera.Simplemjpeg")]
